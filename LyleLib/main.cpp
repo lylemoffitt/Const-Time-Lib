cb7db4fba368c1598fc3ec57ab60ce846a83a513
lylemoffitt <lyle@moffitt.com>
2014-12-16 19:41:26 -0500

    Initial Commit

---
+++ LyleLib/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LyleLib
+//
+//  Created by Lyle Moffitt on 12/16/14.
+//  Copyright (c) 2014 Lyle Moffitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
